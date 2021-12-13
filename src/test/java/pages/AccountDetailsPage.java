15aa89f1f1d25c9dc91f1b316b58bb6a93fff6a4
allaivanova1989 <baby20086@rambler.ru>
2021-12-13 23:25:45 +0300

    Fix Account test

--- src/test/java/pages/AccountDetailsPage.java
+++ src/test/java/pages/AccountDetailsPage.java
@@ -6,14 +6,9 @@ import org.openqa.selenium.WebDriver;
 public class AccountDetailsPage extends BasePage {
     public static final By DETAILS_LINK = By.xpath("//a[@id='detailTab__item']");
     String fieldValue = "//flexipage-tab2//force-record-layout-item//span[text()='%s']/ancestor::force-record-layout-item//slot[@slot=\"outputField\"]//*";
-    String adressLocator = "//div//span[@class=\"test-id__field-label\" and text()='%s']";
 
     public String getFieldValueByName(String fieldName) {
         return driver.findElement(By.xpath(String.format(fieldValue, fieldName))).getText();
-    }
-
-    public String getAddressValueByName(String addressName) {
-        return driver.findElement(By.xpath(String.format(adressLocator, addressName))).getText();
     }
 
     public AccountDetailsPage(WebDriver driver) {
